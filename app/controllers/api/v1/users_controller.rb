c520ec11517c67ee7073cf46cb80adb4fdb78f8a
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-10 00:26:38 -0400

    setup user authentication routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < ApplicationController
+  def show
+    token = request.headers["Authentication"].split(" ")[1]
+    render json: User.find(decode(token)["user_id"]), status: :accepted
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/profile', to: 'users#show'
+      get '/profile', to: 'user#show'
       post '/login', to: 'auth#create'
       resources :lockers
       resources :items
